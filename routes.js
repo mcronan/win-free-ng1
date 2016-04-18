fc3987b8c08177ea7d397dc7436c6d520b0f8837
Ronan <ronanir600@gmail.com>
2016-04-18 22:30:57 +0100

    homepage & login

--- routes.js
+++ routes.js
@@ -40,7 +40,7 @@ module.exports = function(app, passport) {
 	    // process the signup form
 		app.post('/signup', passport.authenticate('local-signup', {
 		        successRedirect : '/profile', // redirect to the secure profile section
-		        failureRedirect : '/index', // redirect back to the signup page if there is an error
+		        failureRedirect : '/', // redirect back to the signup page if there is an error
 		        failureFlash : true, // allow flash messages
 
 		    }));
